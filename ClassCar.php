b04edbb20ea35989a3bf7c644c62c49166a0efa5
HYehor <gorelovegor5@gmail.com>
2019-03-14 10:57:34 +0200

    Create project

---
+++ ClassCar.php
@@ -0,0 +1,20 @@
+<?php
+class Shop
+{
+    private $name;
+    public function naming()
+    {
+        $this->name = 'CCC';
+        echo $this->name;
+    }
+}
+
+class user
+{
+    public $name = 'Rik';
+}
+
+class work
+{
+
+}
